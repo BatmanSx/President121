7feb37bb3eea39048dd8a34ef487d81216189a90
BatmanSx <79669448+BatmanSx@users.noreply.github.com>
2022-02-20 00:29:32 +0100

    a

---
+++ zarzadzanie/build/precache-manifest.f9807ee1b8fae0cc81cd42bf96c5a341.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f0d6bacdb189d87013a2efb726193b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b409c9ee2f825b27f5f3",
+    "url": "/static/css/main.155bebda.chunk.css"
+  },
+  {
+    "revision": "1bd6df90c7acbe6cbba9",
+    "url": "/static/js/2.667e694b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.667e694b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b409c9ee2f825b27f5f3",
+    "url": "/static/js/main.91cf3dce.chunk.js"
+  },
+  {
+    "revision": "90191387203d4dd7249b",
+    "url": "/static/js/runtime-main.ed995257.js"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/menu.b3f49afd.svg"
+  }
+]);
